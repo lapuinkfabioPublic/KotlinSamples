cc48187de6989e241e815b1c6c959bfc6d90acc2
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2026-01-09 18:56:34 -0300

    day

--- TesteUnitarios/src/test/kotlin/PortariaTest.kt
+++ TesteUnitarios/src/test/kotlin/PortariaTest.kt
@@ -1,23 +1,43 @@
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.DisplayName
 import kotlin.test.Test
 
 class PortariaTest  {
 
+    val bemvindo = "Welcome :)"
+    val conviteInvalido = "Negado. Convite inválido."
+    var negadoMenoresIdade = "Negado. Menores de idade não são permitidos."
+
     fun portariaTest(){}
 
     @Test
     @DisplayName("Testa a idade das portarias")
     fun portariaIdades(){
-        portaria(18,"comum", "xt0001")
-        portaria(10,"comum", "xt0002")
-        portaria(20,"comum", "xt0003")
+        Assertions.assertEquals(portaria(18,"comum", "xt0001"), bemvindo)
+        Assertions.assertEquals(portaria(10,"comum", "xt0002"), bemvindo)
+        Assertions.assertEquals(portaria(20,"comum", "xt0003"), bemvindo)
     }
 
     @Test
+    @DisplayName("Negar a entrada para convites inválidos")
+    fun validaTipoDeConvite(){
+        Assertions.assertEquals(portaria(18,"comum", "xl2"),conviteInvalido)
+        Assertions.assertEquals(portaria(10,"dd", "xl2"),conviteInvalido)
+        Assertions.assertEquals(portaria(20,"dd", "xl2"),conviteInvalido)
+    }
+
+    @Test
+    @DisplayName("Testa a idade das portarias")
     fun portaThrow(){
-        Assumptions.assumeTrue(portaria(10,"comum", "xt0001") == "Welcome :)")
-        Assumptions.assumeTrue(portaria(18,"comum", "xt0001") == "Welcome :)")
-        Assumptions.assumeTrue(portaria(20,"comum", "xt0001") == "Welcome :)")
+        Assumptions.assumeTrue(portaria(100,"comum", "xt0001") == bemvindo )
+        Assumptions.assumeTrue(portaria(18,"comum", "xt0001") == bemvindo)
+        Assumptions.assumeTrue(portaria(20,"comum", "xt0001") == bemvindo)
+
+        Assertions.assertEquals(portaria(100,"comum", "xt0001") , bemvindo)
+        Assertions.assertEquals(portaria(18,"comum", "xt0001") , bemvindo)
+        Assertions.assertEquals(portaria(20,"comum", "xt0001") , bemvindo)
+
+        //
     }
 }
