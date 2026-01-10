48fb9a3a444c309b5838668fd7dd0d48b3dadde6
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2026-01-10 15:25:10 -0300

    day

--- TesteUnitarios/src/main/kotlin/Main.kt
+++ TesteUnitarios/src/main/kotlin/Main.kt
@@ -4,19 +4,30 @@ fun endereco(rua :String, cidade :String, estado : String,
 
 
 }
-
-fun media(vararg valores:Float): Float
+/*
+fun abc(): Nothing
+{
+    //TODO para fazer
+}
+*/
+fun media(vararg valores: Any): Float
 {
     var soma = 0F
     for (i in valores)
     {
-        soma += i
+        when (i) {
+            is Float -> soma += i
+//            is Double -> soma += i
+            is Int -> soma += i
+  //          is String -> soma += i
+        }
     }
     return soma / valores.size
 }
 
 fun main(){
     println("A média é:%f".format(media(8F, 10F, 5f, 5f)))
+    println("A média é:%f".format(media(1, 2, 3, 4)))
 
     /*endereco("", "", estado = "")
 
