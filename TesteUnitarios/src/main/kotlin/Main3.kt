4a9fa4f8fc3426516e8b3bf813ce15b5308d1588
Fabio Leandro Lapuinka <lapuinkafabio@gmail.com>
2026-01-13 00:11:01 -0300

    day

--- TesteUnitarios/src/main/kotlin/Main3.kt
+++ TesteUnitarios/src/main/kotlin/Main3.kt
@@ -3,39 +3,44 @@ fun main(){
     var c1 : Computador
     var m1:Microfone
 
-    c1 = Computador("Dell", 100F)
+    c1 = Computador("Dell", 100F , 1)
     m1 = Microfone("Aiwa", 30F)
+
+    Computador("IBM", 100F , 1).instalarSoftware()
 
     println(c1.marca)
     println(c1.preco)
-    c1.ligar()
-    c1.desligar()
+    c1.instalarSoftware()
 
-
-    m1.ligar()
     m1.aumentarVolume()
-
-
     println(m1.marca)
     println(m1.preco)
 
 }
 
-class Computador ( marca:String,  preco: Float): Eletronico(marca, preco) {
-    fun instalarSoftware(){}
+public class Computador ( marca:String,  preco: Float, cor: Int): Eletronico(marca, preco) {
+    fun instalarSoftware(){
+        super.ligar()
+        println("Instalando...")
+        println("Instalado")
+        super.desligar()
+    }
+
     fun removerSoftware(){}
 }
 
-class Microfone(marca: String, preco: Float) : Eletronico(marca, preco) {
-    fun aumentarVolume(){}
-    fun diminuirVolume(){}
-    fun mute(){}
-    fun unmute(){}
+public class Microfone(marca: String, preco: Float) : Eletronico(marca, preco) {
+    public fun aumentarVolume(){}
+    public fun diminuirVolume(){}
+    public fun mute(){}
+    public fun unmute(){}
+    public fun gravar(){}
 }
 
-open class Eletronico(var marca:String, var preco:Float){
-    fun ligar(){}
-    fun desligar(){}
+public  open class Eletronico(var marca:String, var preco:Float){
+    //public , private , protected
+    protected fun ligar(){}
+    protected fun desligar(){}
 
 }
 
